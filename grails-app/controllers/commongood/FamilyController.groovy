36cfbe8d5b9c6f37330a29d68ea5e313d78c6547
Mark Gordon <canuckisman@gmail.com>
2015-12-23 16:07:41 -0700

    Moved "save family" from SaveFamilyController to FamilyController First code that actually writes new or changed family to db

--- grails-app/controllers/commongood/FamilyController.groovy
+++ grails-app/controllers/commongood/FamilyController.groovy
@@ -1,6 +1,51 @@
 package commongood
 
 class FamilyController {
-    def scaffold = true
-    def index() { }
+//  static allowedMethods = [index:'POST']
+
+// http://localhost:8080/CommonGood/family/save/2?name=Wertzworth&participateInInterview=F&permissionToContact=T&note=Good+times
+// http://localhost:8080/CommonGood/navigate/family/2
+
+    def save() {
+        def addressId = Long.valueOf( params.addressId )
+        def interviewerId = Long.valueOf( params.interviewerId )
+        def name = params.familyName
+        def participate = ('participateInInterview' in params)
+        def permission = ('permissionToContact' in params)
+        def interviewDate = new Date( ).parse( 'yyyy-MM-dd', params.initialInterviewDate )
+        def note = params.note
+        def orderWithin = params.orderWithinAddress
+
+        println "familyName=${name} participateInInterview=${participate} permissionToContact=${permission} note=${note}"
+
+        if( 'id' in params ) {
+            println "Request to CHANGE family ${familyId}"
+            // The request wants us to change an existing family
+            def familyId = Long.valueOf( params.id )
+            def family = Family.get( familyId )
+            faily.address = Address.get( addressId )
+            family.interviewer = Person.get( interviewerId )
+            family.name = name
+            family.participateInInterview = participate
+            family.permissionToContact = permission
+            family.note = note
+            family.interviewDate = interviewDate
+            family.order = orderWithin
+            // TODO Test: family.save(failOnError: true)
+            family.save( flush:true )
+            forward controller:'navigate', action:'family', id:familyId
+        } else {
+            // The request is to create a new family
+            Family family = new Family( )
+            faily.address = Address.get( addressId )
+            family.interviewer = Person.get( interviewerId )
+            family.name = name
+            family.participateInInterview = participate
+            family.permissionToContact = permission
+            family.note = note
+            family.interviewDate = interviewDate
+            family.order = orderWithin
+            family.save( flush:true )
+        }
+    }
 }
--- grails-app/controllers/commongood/SaveFamilyController.groovy
+++ grails-app/controllers/commongood/SaveFamilyController.groovy
@@ -1,41 +1,8 @@
 package commongood
 
 class SaveFamilyController {
-//  static allowedMethods = [index:'POST']
-
-// http://localhost:8080/CommonGood/saveFamily/index/2?name=Wertzworth&participateInInterview=F&permissionToContact=T&note=Good+times
-// http://localhost:8080/CommonGood/navigate/family/2
-
     def index() {
-        def name = params.familyName
-        def participate = ('participateInInterview' in params)
-        def permission = ('permissionToContact' in params)
-        def inDate = new Date( params.initialInterviewDate )
-        def note = params.note
-        def orderWithin = params.orderWithinAddress
-
-        println "familyName=${name} participateInInterview=${participate} permissionToContact=${permission} note=${note}"
-
-        if( 'id' in params ) {
-            println "Request to CHANGE family ${familyId}"
-            // The request wants us to change an existing family
-            def familyId = Long.valueOf( params.id )
-            def family = Family.get( familyId )
-            family.name = name
-            family.participateInInterview = participate
-            family.permissionToContact = permission
-            family.note = note
-            family.interviewDate = inDate
-            family.order = orderWithin
-            // TODO Test: family.save(failOnError: true)
-            family.errors.each {
-               println it
-            }
-            family.save(flush: true)
-            forward controller:'navigate', action:'family', id:familyId
-        } else {
-            // The request is to create a new family
-            render 'Request to CREATE a new family -- not implemented'
-        }
+        println "Params: ${params}"
+        render 'SaveFamilyController replaced by FamilyController: URL is family/save/123?this=that&etc=moreStuff'
     }
 }
