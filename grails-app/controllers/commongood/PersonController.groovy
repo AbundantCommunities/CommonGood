7e41fee1e79cc3797ccfe11a080e0e728cc16be0
Mark Gordon <canuckisman@gmail.com>
2015-12-28 00:12:28 -0700

    PersonController can change a family member and edit a family member.

--- grails-app/controllers/commongood/PersonController.groovy
+++ grails-app/controllers/commongood/PersonController.groovy
@@ -4,26 +4,27 @@ class PersonController {
     static allowedMethods = [index:'POST']
 
     def save() {
+        Person person
         if( 'id' in params ) {
             def personId = Long.valueOf( params.id )
             println "Request to CHANGE person ${personId}"
-            def person = Person.get( personId )
+            person = Person.get( personId )
         } else {
             // The request is to create a new person
             def familyId = Long.valueOf( params.familyId )
             println "Request to ADD a person to family ${familyId}"
-            Person person = new Person( )
+            person = new Person( )
             person.family = Family.get( familyId )
         }
 
         person.firstNames = params.firstNames
         person.lastName = params.lastName
-        person.birthYear = params.birthYear
+        person.birthYear = Integer.valueOf( params.birthYear )
         person.emailAddress = params.emailAddress
         person.phoneNumber = params.phoneNumber
-        person.orderWithinFamily = params.orderWithinFamily
+        person.orderWithinFamily = Integer.valueOf( params.orderWithinFamily )
         // TODO Test: family.save(failOnError: true)
         person.save( flush:true )
-        forward controller:'navigate', action:'familymember', id:personId
+        forward controller:'navigate', action:'familymember', id:person.id
     }
 }
