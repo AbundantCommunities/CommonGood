48a8d0da828b901caf2413726df4ac1028ac4edb
Mark Gordon <canuckisman@gmail.com>
2015-12-30 09:38:43 -0700

    DomainAuthorizationService now correctly tags current interviewer.

--- grails-app/services/commongood/DomainAuthorizationService.groovy
+++ grails-app/services/commongood/DomainAuthorizationService.groovy
@@ -5,33 +5,54 @@ import grails.transaction.Transactional
 @Transactional
 class DomainAuthorizationService {
     /**
-     * For a given block in a given neighbourhood, find the people who might have
-     * the right to interview someone. That will be the block's BCs and the neighnourhood's
-     * NCs.
+     * For a given block in a given neighbourhood, find the people who might
+     * have interviewed a resident of that block. That will be the block's
+     * connectors plus the neighourhood's connectors.
      * 
-     * If the caller knows a previously selected interviewer (person) id, then pass that
-     * in as currentInterviewerId -- we'll return a value of checked set to 'checked'.
+     * If the caller knows the currentInterviewerId (person id of the interviewer)
+     * then our result will highlight that person (default entry in map is true).
      * 
-     * @return List of [ id:personId, fullName:personFullName, checked:thisPersonAlreadySelected ]
+     * @return List of [ id:personId, fullName:personFullName, default:thisPersonAlreadySelected ]
     */
     def getPossibleInterviewers( neighbourhoodId, blockId, currentInterviewerId ) {
+        // TODO Would code be cleaner if caller passed in Block instead of nh & blk ids?
         Integer iNeighbourhoodId = neighbourhoodId
         Integer iBlockId = blockId
 
         def result = [ ]
 
+        // Who are the neighbourhood connectors?
         def das = DomainAuthorization.findAll("from DomainAuthorization da join da.person where da.domainCode=? and da.domainKey=?",
                     [ DomainAuthorization.NEIGHBOURHOOD, , iNeighbourhoodId ])
-
-        das = DomainAuthorization.findAll("from DomainAuthorization da join da.person where da.domainCode=? and da.domainKey=?",
-                    [ DomainAuthorization.BLOCK, , iBlockId ])
 
         das.each {
             // each result row found above has a list like [ DomainAuthorization, Person ]
             def thisOne = (it[1].id == currentInterviewerId) ? 'true' : 'false'
-            result << [ id: it[1].id, fullName:it[1].fullName, checked:thisOne ]
+            result << [ id: it[1].id, fullName:it[1].fullName, 'default':thisOne ]
         }
 
+        // Who are the block connectors?
+        das = DomainAuthorization.findAll("from DomainAuthorization da join da.person where da.domainCode=? and da.domainKey=?",
+                    [ DomainAuthorization.BLOCK, , iBlockId ])
+
+        das.each {
+            def thisOne = (it[1].id == currentInterviewerId) ? 'true' : 'false'
+            result << [ id: it[1].id, fullName:it[1].fullName, 'default':thisOne ]
+        }
+
+        if( currentInterviewerId ) {
+            // It is possible that result does not contain currentInterviewerId
+            def findResult = result.find{ it.id == currentInterviewerId }
+            println "currentInterviewerId is non null; find result is ${findResult}"
+            if( !findResult ) {
+                def interviewer = Person.get( currentInterviewerId )
+                result << [ id:currentInterviewerId, fullName:interviewer.fullName, 'default':true ]
+            }
+        } else {
+            println "currentInterviewerId is null"
+        }
+
+        println "Possible Interviewers are ${result}"
         return result
     }
     
