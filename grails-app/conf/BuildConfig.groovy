a1f27bd773843ca2da47e17b5ca53a46560a93b8
Mark Gordon <canuckisman@gmail.com>
2016-01-12 14:52:02 -0700

    We now use Grails's cors plugin to support a league website calling to CG for ACI answers. We need to set up CommonGood/public and CommonGood/api paths and limit CORS.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -63,6 +63,9 @@ grails.project.dependency.resolution = {
         // asset-pipeline 2.0+ requires Java 7, use version 1.9.x with Java 6
         compile ":asset-pipeline:2.5.7"
 
+        // Support league websites calling for live ACI info
+        compile ":cors:1.1.8"
+
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.10" // or ":hibernate:3.6.10.18"
         runtime ":database-migration:1.4.0"
