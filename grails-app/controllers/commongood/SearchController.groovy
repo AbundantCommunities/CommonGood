755353218cfab1ee91535ac89644e3155b0174e8
Mark Gordon <canuckisman@gmail.com>
2016-03-14 15:52:28 -0600

    Finished enhancing search of people & answers so that BCs see only results from their block.

--- grails-app/controllers/commongood/SearchController.groovy
+++ grails-app/controllers/commongood/SearchController.groovy
@@ -7,8 +7,8 @@ class SearchController {
         // No need to ask AuthorizationService about permission.
         // SearchService limits results to NH of currently signed in user.
         log.info "${session.user.getLogName()} ${session.authorized} searching for '${params.q}'"
-        def answers = searchService.answers( session, params.q )
         def people = searchService.people( session, params.q )
+        def answers = searchService.answers( session, params.q )
         return [ q:params.q, answers:answers, people:people ]
     }
 
@@ -16,8 +16,8 @@ class SearchController {
         // No need to ask AuthorizationService about permission.
         // SearchService limits results to NH of currently signed in user.
         log.info "${session.user.getLogName()} ${session.authorized} searching for '${params.q}' (with contact info)"
-        def answers = searchService.answersWithContactInfo( session, params.q )
         def people = searchService.peopleWithContactInfo( session, params.q )
+        def answers = searchService.answersWithContactInfo( session, params.q )
         return [ q:params.q, answers:answers, people:people ]
     }
 }
--- grails-app/services/commongood/SearchService.groovy
+++ grails-app/services/commongood/SearchService.groovy
@@ -15,9 +15,9 @@ class SearchService {
             answers = Answer.executeQuery(
                 'select ans.text, ans.wouldLead, ans.wouldOrganize, p.id, p.firstNames, p.lastName, q.shortText \
                  from Answer ans, Person p, Question q \
-                 where lower(a.text) like ? \
-                 AND a.person.id = p.id \
-                 AND a.question.id = q.id \
+                 where LOWER(ans.text) like ? \
+                 AND ans.person.id = p.id \
+                 AND ans.question.id = q.id \
                  AND q.neighbourhood.id = ? \
                  ORDER BY p.firstNames, p.lastName, p.id',
                 [ searchTerm, neighbourhoodId ])
@@ -27,7 +27,7 @@ class SearchService {
             answers = Answer.executeQuery(
                 'SELECT ans.text, ans.wouldLead, ans.wouldOrganize, p.id, p.firstNames, p.lastName, q.shortText \
                  FROM Answer ans, Person p, Family f, Address addr, Question q \
-                 WHERE lower(ans.text) LIKE ? \
+                 WHERE LOWER(ans.text) LIKE ? \
                  AND ans.person.id = p.id \
                  AND ans.question.id = q.id \
                  AND p.family.id = f.id \
@@ -42,34 +42,56 @@ class SearchService {
     }
 
     def answersWithContactInfo( session, q ) {
-        def neighbourhoodId = session.neighbourhood.id
-        log.info "Searching NH ${neighbourhoodId} answers for ${q}"
         def searchTerm = "%${q}%".toLowerCase( )
         def answers
 
-        answers = Answer.executeQuery(
-            'SELECT ans.text, ans.wouldLead, ans.wouldOrganize, p.id, p.firstNames, p.lastName, q.shortText, \
-             p.phoneNumber, p.emailAddress, addr.text \
-             FROM Answer ans, Person p, Family f, Address addr, Question q \
-             WHERE lower(ans.text) like ? \
-             AND ans.person.id = p.id \
-             AND ans.question.id = q.id \
-             AND q.neighbourhood.id = ? \
-             AND p.family.id = f.id \
-             AND f.address.id = addr.id \
-             ORDER BY p.firstNames, p.lastName, p.id',
-            [ searchTerm, neighbourhoodId ])
+        if( session.authorized.forNeighbourhood() ) {
+            def neighbourhoodId = session.neighbourhood.id
+            log.info "${session.user.logName} searching neighbourhood ${neighbourhoodId} answers for ${q} (with contact info)"
+
+            answers = Answer.executeQuery(
+                'SELECT ans.text, ans.wouldLead, ans.wouldOrganize, p.id, p.firstNames, p.lastName, q.shortText, \
+                 p.phoneNumber, p.emailAddress, addr.text \
+                 FROM Answer ans, Person p, Family f, Address addr, Question q \
+                 WHERE LOWER(ans.text) like ? \
+                 AND ans.person.id = p.id \
+                 AND ans.question.id = q.id \
+                 AND q.neighbourhood.id = ? \
+                 AND p.family.id = f.id \
+                 AND f.address.id = addr.id \
+                 ORDER BY p.firstNames, p.lastName, p.id',
+                [ searchTerm, neighbourhoodId ])
+        } else {
+            def blockId = session.block.id
+            log.info "${session.user.logName} searching block ${blockId} answers for ${q} (with contact info)"
+
+            answers = Answer.executeQuery(
+                'SELECT ans.text, ans.wouldLead, ans.wouldOrganize, p.id, p.firstNames, p.lastName, q.shortText, \
+                 p.phoneNumber, p.emailAddress, addr.text \
+                 FROM Answer ans, Person p, Family f, Address addr, Question q \
+                 WHERE LOWER(ans.text) like ? \
+                 AND ans.person.id = p.id \
+                 AND ans.question.id = q.id \
+                 AND p.family.id = f.id \
+                 AND f.address.id = addr.id \
+                 AND addr.block.id = ? \
+                 ORDER BY p.firstNames, p.lastName, p.id',
+                [ searchTerm, blockId ])
+        }
 
         log.info "Found ${answers.size()} answers"
         return answers
     }
 
     def people( session, q ) {
-        def neighbourhoodId = session.neighbourhood.id
-        log.info "Searching NH ${neighbourhoodId} people for ${q}"
         def searchTerm = "%${q}%".toLowerCase( )
+        def peeps
 
-        def peeps = Person.executeQuery(
+        if( session.authorized.forNeighbourhood() ) {
+            def neighbourhoodId = session.neighbourhood.id
+            log.info "${session.user.logName} searching neighbourhood ${neighbourhoodId} people for ${q}"
+
+            peeps = Person.executeQuery(
             'select p.id, p.firstNames, p.lastName \
              from Person p join p.family f join f.address a \
              where (lower(a.text) like ? or lower(a.note) like ? or lower(f.name) like ? or lower(f.note) like ? \
@@ -77,22 +99,54 @@ class SearchService {
              or lower(p.note) like ?) and a.block.neighbourhood.id = ? order by p.firstNames, p.lastName, p.id',
             ([ searchTerm ] * 9) << neighbourhoodId )
 
+        } else {
+            def blockId = session.block.id
+            log.info "${session.user.logName} searching block ${blockId} people for ${q}"
+
+            peeps = Person.executeQuery(
+            'select p.id, p.firstNames, p.lastName \
+             from Person p join p.family f join f.address a \
+             where (lower(a.text) like ? or lower(a.note) like ? or lower(f.name) like ? or lower(f.note) like ? \
+             or lower(p.firstNames) like ? or lower(p.lastName) like ? or lower(p.phoneNumber) like ? or lower(p.emailAddress) like ? \
+             or lower(p.note) like ?) \
+             and a.block.id = ? \
+             order by p.firstNames, p.lastName, p.id',
+            ([ searchTerm ] * 9) << blockId )
+        }
+
         log.info "Found ${peeps.size()} people"
         return peeps
     }
 
     def peopleWithContactInfo( session, q ) {
-        def neighbourhoodId = session.neighbourhood.id
-        log.info "Searching NH ${neighbourhoodId} people for ${q}"
         def searchTerm = "%${q}%".toLowerCase( )
+        def peeps
 
-        def peeps = Person.executeQuery(
-            'select p.id, p.firstNames, p.lastName, p.phoneNumber, p.emailAddress, a.text \
-             from Person p join p.family f join f.address a \
-             where (lower(a.text) like ? or lower(a.note) like ? or lower(f.name) like ? or lower(f.note) like ? \
-             or lower(p.firstNames) like ? or lower(p.lastName) like ? or lower(p.phoneNumber) like ? or lower(p.emailAddress) like ? \
-             or lower(p.note) like ?) and a.block.neighbourhood.id = ? order by p.firstNames, p.lastName, p.id',
-            ([ searchTerm ] * 9) << neighbourhoodId )
+        if( session.authorized.forNeighbourhood() ) {
+            def neighbourhoodId = session.neighbourhood.id
+            log.info "Searching NH ${neighbourhoodId} people for ${q} (with contact info)"
+
+            peeps = Person.executeQuery(
+                'select p.id, p.firstNames, p.lastName, p.phoneNumber, p.emailAddress, a.text \
+                 from Person p join p.family f join f.address a \
+                 where (LOWER(a.text) like ? OR LOWER(a.note) like ? OR LOWER(f.name) like ? OR LOWER(f.note) like ? \
+                 OR LOWER(p.firstNames) like ? OR LOWER(p.lastName) like ? OR LOWER(p.phoneNumber) like ? OR LOWER(p.emailAddress) like ? \
+                 OR LOWER(p.note) like ?) and a.block.neighbourhood.id = ? order by p.firstNames, p.lastName, p.id',
+                ([ searchTerm ] * 9) << neighbourhoodId )
+        } else {
+            def blockId = session.block.id
+            log.info "Searching block ${blockId} people for ${q} (with contact info)"
+
+            peeps = Person.executeQuery(
+                'select p.id, p.firstNames, p.lastName, p.phoneNumber, p.emailAddress, a.text \
+                 from Person p join p.family f join f.address a \
+                 where (LOWER(a.text) like ? OR LOWER(a.note) like ? OR LOWER(f.name) like ? OR LOWER(f.note) like ? \
+                 OR LOWER(p.firstNames) like ? OR LOWER(p.lastName) like ? OR LOWER(p.phoneNumber) like ? OR LOWER(p.emailAddress) like ? \
+                 OR LOWER(p.note) like ?) \
+                 AND a.block.id = ? \
+                 order by p.firstNames, p.lastName, p.id',
+                ([ searchTerm ] * 9) << blockId )
+        }
 
         log.info "Found ${peeps.size()} people"
         return peeps
