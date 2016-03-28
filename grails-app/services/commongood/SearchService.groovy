474b446d2874b2ef6252bee668fe50cd5e3a75d5
theguy <theguy@192.168.0.106>
2016-03-28 12:02:21 -0600

    We now include answer.note when we search (not yet passing the note to the GSP).

--- grails-app/services/commongood/SearchService.groovy
+++ grails-app/services/commongood/SearchService.groovy
@@ -19,26 +19,26 @@ class SearchService {
             answers = Answer.executeQuery(
                 'SELECT ans.text, ans.wouldAssist, p.id, p.firstNames, p.lastName, q.shortText \
                  from Answer ans, Person p, Question q \
-                 where LOWER(ans.text) like ? \
+                 WHERE (LOWER(ans.text) LIKE ? OR LOWER(ans.note) LIKE ?) \
                  AND ans.person.id = p.id \
                  AND ans.question.id = q.id \
                  AND q.neighbourhood.id = ? \
                  ORDER BY p.firstNames, p.lastName, p.id',
-                [ searchTerm, neighbourhoodId ])
+                [searchTerm] * 2 << neighbourhoodId )
         } else {
             def blockId = session.block.id
             log.info "${session.user.logName} searching block ${blockId} answers for ${q}"
             answers = Answer.executeQuery(
                 'SELECT ans.text, ans.wouldAssist, p.id, p.firstNames, p.lastName, q.shortText \
                  FROM Answer ans, Person p, Family f, Address addr, Question q \
-                 WHERE LOWER(ans.text) LIKE ? \
+                 WHERE (LOWER(ans.text) LIKE ? OR LOWER(ans.note) LIKE ?) \
                  AND ans.person.id = p.id \
                  AND ans.question.id = q.id \
                  AND p.family.id = f.id \
                  AND f.address.id = addr.id \
                  AND addr.block.id = ? \
                  ORDER BY p.firstNames, p.lastName, p.id',
-                [ searchTerm, blockId ])
+                [searchTerm] * 2 << blockId )
         }
 
         log.info "Found ${answers.size()} answers"
@@ -57,14 +57,14 @@ class SearchService {
                 'SELECT ans.text, ans.wouldAssist, p.id, p.firstNames, p.lastName, q.shortText, \
                  p.phoneNumber, p.emailAddress, addr.text \
                  FROM Answer ans, Person p, Family f, Address addr, Question q \
-                 WHERE LOWER(ans.text) like ? \
+                 WHERE (LOWER(ans.text) LIKE ? OR LOWER(ans.note) LIKE ?) \
                  AND ans.person.id = p.id \
                  AND ans.question.id = q.id \
                  AND q.neighbourhood.id = ? \
                  AND p.family.id = f.id \
                  AND f.address.id = addr.id \
                  ORDER BY p.firstNames, p.lastName, p.id',
-                [ searchTerm, neighbourhoodId ])
+                [searchTerm] * 2 << neighbourhoodId )
         } else {
             def blockId = session.block.id
             log.info "${session.user.logName} searching block ${blockId} answers for ${q} (with contact info)"
@@ -73,14 +73,14 @@ class SearchService {
                 'SELECT ans.text, ans.wouldAssist, p.id, p.firstNames, p.lastName, q.shortText, \
                  p.phoneNumber, p.emailAddress, addr.text \
                  FROM Answer ans, Person p, Family f, Address addr, Question q \
-                 WHERE LOWER(ans.text) like ? \
+                 WHERE (LOWER(ans.text) LIKE ? OR LOWER(ans.note) LIKE ?) \
                  AND ans.person.id = p.id \
                  AND ans.question.id = q.id \
                  AND p.family.id = f.id \
                  AND f.address.id = addr.id \
                  AND addr.block.id = ? \
                  ORDER BY p.firstNames, p.lastName, p.id',
-                [ searchTerm, blockId ])
+                [searchTerm] * 2 << blockId )
         }
 
         log.info "Found ${answers.size()} answers"
@@ -101,7 +101,7 @@ class SearchService {
              where (lower(a.text) like ? or lower(a.note) like ? or lower(f.name) like ? or lower(f.note) like ? \
              or lower(p.firstNames) like ? or lower(p.lastName) like ? or lower(p.phoneNumber) like ? or lower(p.emailAddress) like ? \
              or lower(p.note) like ?) and a.block.neighbourhood.id = ? order by p.firstNames, p.lastName, p.id',
-            ([ searchTerm ] * 9) << neighbourhoodId )
+            [searchTerm] * 9 << neighbourhoodId )
 
         } else {
             def blockId = session.block.id
@@ -115,7 +115,7 @@ class SearchService {
              or lower(p.note) like ?) \
              and a.block.id = ? \
              order by p.firstNames, p.lastName, p.id',
-            ([ searchTerm ] * 9) << blockId )
+            [searchTerm] * 9 << blockId )
         }
 
         log.info "Found ${peeps.size()} people"
@@ -136,7 +136,7 @@ class SearchService {
                  where (LOWER(a.text) like ? OR LOWER(a.note) like ? OR LOWER(f.name) like ? OR LOWER(f.note) like ? \
                  OR LOWER(p.firstNames) like ? OR LOWER(p.lastName) like ? OR LOWER(p.phoneNumber) like ? OR LOWER(p.emailAddress) like ? \
                  OR LOWER(p.note) like ?) and a.block.neighbourhood.id = ? order by p.firstNames, p.lastName, p.id',
-                ([ searchTerm ] * 9) << neighbourhoodId )
+                [searchTerm] * 9 << neighbourhoodId )
         } else {
             def blockId = session.block.id
             log.info "Searching block ${blockId} people for ${q} (with contact info)"
@@ -149,7 +149,7 @@ class SearchService {
                  OR LOWER(p.note) like ?) \
                  AND a.block.id = ? \
                  order by p.firstNames, p.lastName, p.id',
-                ([ searchTerm ] * 9) << blockId )
+                [searchTerm] * 9 << blockId )
         }
 
         log.info "Found ${peeps.size()} people"
