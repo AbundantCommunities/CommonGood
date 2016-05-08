5ef5d0f890dae15695c893d397ca327962f424c1
theguy <theguy@192.168.0.106>
2016-05-08 14:36:28 -0600

    set all passwords to shh

--- scripts/handy.sql
+++ scripts/handy.sql
@@ -1,4 +1,10 @@
 /*
+On your TEST DATABASE ONLY, set everyone's password hash to shh
+UPDATE person SET hashed_password =
+'PBKDF2WithHmacSHA512|75000|64|256|o6auh3NPvmO/E5uLDV2AGxyDj7J6pBtc+kFrAUVt8QbKc40sx8w9/qpnh3x1V1HxgHhNXCrpuPmADqQiY8DX5w==|QK1tbv8gYSgcq6zddVFHI6XaVYKb5K9SAGuC4jHuOes=';
+*/
+
+/*
 CREATE USER myapp PASSWORD 'sloj92GOM';
 CREATE DATABASE thehoods WITH OWNER myapp;
 DROP DATABASE thehoods;
