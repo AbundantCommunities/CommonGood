08d5c6300b8737924056f0ff8410bb13d3850f3a
theguy <theguy@192.168.0.106>
2016-07-26 19:16:35 -0600

    Now we also include /etc/letsencrypt in our backups.

--- production/backup.bash
+++ production/backup.bash
@@ -5,3 +5,9 @@
 cd ~/backup
 pg_dump --format=c --disable-triggers thehoods > database.dmp
 openssl smime -encrypt -aes256 -binary -outform DEM -out database.dmp.encrypted backup_key.pem.pub < database.dmp
+
+openssl smime -encrypt -aes256 -binary -outform DEM -out fullchain_and_key.p12.encrypted backup_key.pem.pub < ~/fullchain_and_key.p12
+
+sudo tar zcf letsencrypt.tgz --directory /etc letsencrypt
+openssl smime -encrypt -aes256 -binary -outform DEM -out letsencrypt.tgz.encrypted backup_key.pem.pub < letsencrypt.tgz
+rm --force letsencrypt.tgz
