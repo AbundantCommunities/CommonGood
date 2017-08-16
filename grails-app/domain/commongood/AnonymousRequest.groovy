247cef5ba2d77a62f07ccff99fcdfc75be73dce7
cognish <mark@cognish.com>
2017-08-16 16:30:38 -0600

    Improved documentation in AnonymousRequest

--- grails-app/domain/commongood/AnonymousRequest.groovy
+++ grails-app/domain/commongood/AnonymousRequest.groovy
@@ -2,19 +2,27 @@ package commongood
 
 /**
  * Intent is that a neighbourhood's website hosts a form into which a resident
- * enters a request like "I want to make garden with my neighbours".
+ * enters a request like "I want to garden with my neighbours".
  */
 class AnonymousRequest {
+     // These fields could identify anybody or nobody.
+     // They COULD identify someone in CG's Person table.
     String residentName
     String homeAddress
     String emailAddress
     String phoneNumber
     String comment
 
+    // We will ensure that this n'hood is in our database
     Neighbourhood neighbourhood
+
+    // Do NOT ASSUME these values will make sense!
+    // The submitting machine can stick anything in these 2 fields!
+    String requestContext // Ex: "activities" for n'hood's Activities page
+    String requestReference // Ex: "birdwatching"
+
+    // This is hard to spoof, but with VPNs and the like, may not be useful.
     String ipAddress
-    String requestContext // For the hosting website to say what page generated request
-    String requestReference // For host to add specificity to requestContext
 
     Date dateCreated
 
