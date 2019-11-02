3dd4b1dcad5ddd2331c04b98f10603495eb4f093
cognish <mark@cognish.com>
2019-11-02 12:59:01 -0600

    added lat+long to Address

--- grails-app/domain/commongood/Address.groovy
+++ grails-app/domain/commongood/Address.groovy
@@ -3,12 +3,20 @@ package commongood
 class Address {
     Block block
     String text // In YEG this is like '9355 93 Ave'
-    String note // Anything peculiar about accessing this address?
+    String note // Ex: 'Basement apartment; door on north side'
     Integer orderWithinBlock
+
+    BigDecimal latitude   // in degrees; negative is south of equator
+    BigDecimal longitude  // in degrees; negative is west of Greenwich
 
     Date dateCreated
     Date lastUpdated
 
-    // In some neighbourhoods addresses have a single Family but not all.
+    // In many neighbourhoods an Address has a single Family, but not all.
     static hasMany = [ families:Family ]
+
+    static constraints = {
+        latitude  min: new BigDecimal('-90'),  max: new BigDecimal('90'),  scale: 5
+        longitude min: new BigDecimal('-180'), max: new BigDecimal('180'), scale: 5
+    }
 }
