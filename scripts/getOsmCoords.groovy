1f23dea4327eae66b053d30298826f7422efde0c
cognish <mark@cognish.com>
2019-12-07 14:31:06 -0700

    geolocate script is a bit tighter

--- scripts/getOsmCoords.groovy
+++ scripts/getOsmCoords.groovy
@@ -5,9 +5,8 @@ import groovy.json.JsonSlurper
 
 def sql = Sql.newInstance('jdbc:postgresql://localhost/thehoods', 'myapp', 'sloj92GOM', 'org.postgresql.Driver')
 def rows = sql.rows('SELECT a.id AS adr_id, a.text, gr.id AS gr_id FROM geolocate_request AS gr, address AS a WHERE a.id = gr.address_id ORDER BY gr.id')
-// sql.connection.close( )
 
-println "Retrieved ${rows.size()} addresses"
+println "Retrieved ${rows.size()} addresses from Geolocate queue"
 def jsonParser = new JsonSlurper( )
 
 def http = configure {
@@ -25,27 +24,26 @@ rows.each{
     def json = responseContent
     if( json.size() == 1 ) {
         if( (json[0].class=='building') || (json[0].class=='place') ) {
-            println "${address}, ${json[0].lat}, ${json[0].lon}"
+            println "Success: address ${it.adr_id}:${address} at ${json[0].lat}, ${json[0].lon}"
 
             sql.execute \
                 'UPDATE address SET latitude=?, longitude=?, geolocate_state=? WHERE id=?', 
                 [ new BigDecimal(json[0].lat), new BigDecimal(json[0].lon), 'S', it.adr_id ]
 
         } else {
-            println "ERROR 'class' is neither 'building' nor 'place' for ${it.adr_id} ${address}: ${json}"
+            println "FAILED: 'class' is neither 'building' nor 'place' for ${it.adr_id}:${address}\n${json}"
             sql.execute \
                 'UPDATE address SET geolocate_state=? WHERE id=?', [ 'F', it.adr_id ]
         }
     } else {
-        println "ERROR ${json.size()} nominatim item(s) retturned for ${it.adr_id} ${address}: ${json}"
+        println "FAILED: ${json.size()} nominatim item(s) returned for ${it.adr_id}:${address}\n${json}"
         sql.execute \
             'UPDATE address SET geolocate_state=? WHERE id=?', [ 'F', it.adr_id ]
     }
 
-    sql.execute \
-        'DELETE FROM geolocate_request WHERE id=?', [ it.gr_id ]
+    sql.execute 'DELETE FROM geolocate_request WHERE id=?', [ it.gr_id ]
 
     sleep 10 * 1000
   }
 
-return
+println 'Normal exit for geolocate script'
